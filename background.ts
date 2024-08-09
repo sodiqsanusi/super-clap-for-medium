b92cbebcde6ac668b21c1fec733b160002d8f3b6
Sodiq 'Ade' Sanusi <ademilddiq@gmail.com>
2024-08-09 22:33:40 +0100

    refactor: not sure why the error is coming up. trying to prevent it from happening

--- background.ts
+++ background.ts
@@ -1,5 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
-  if (tab.url) {
+  const match_article = /https?:\/\/(medium\.com\/(p\/[a-zA-Z0-9]+|@[\w-]+\/[\w-]+|[\w-]+\/[\w-]+)|[\w-]+\.medium\.com\/[\w-]+)/;
+  if (match_article.test(tab.url)) {
     chrome.tabs.sendMessage(tabId, {
       lilac: tab.url,
     });
